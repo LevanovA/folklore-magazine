6d209a0e639f5cb6653837fd9b5439dc6c67c429
Artem <tema.levanov@gmail.com>
2021-01-28 16:22:30 +0300

    Adds adaptive for phone

--- build/js/main.js
+++ build/js/main.js
@@ -8,6 +8,32 @@ $(document).ready(function(){
     });
 });
 
+'use strict';
+
+$(document).ready(function(){
+
+    $('.js-submenu').on('click', function(e){
+        e.preventDefault();
+    });
+
+    $('.js-submenu').on('click', function(e){
+        if ($(this).parent('.menu__item').find('.menu__sublist').is(":visible")) {
+            $('.menu__item').removeClass('active');
+            $('.menu__item .menu__sublist').slideUp(400);
+        } else {
+            $('.menu__item').removeClass('active');
+            $('.menu__item .menu__sublist').slideUp(400);
+            $(this).parent('.menu__item').toggleClass('active');
+            $(this).parent('.menu__item').find('.menu__sublist').slideToggle(400);
+        }
+    });
+
+    $('.menu__subitem').on('click', function(e){
+        $('.menu__item').removeClass('active');
+        $('.menu__item .menu__sublist').slideUp(400);
+    });
+});
+
 'use strick'
 
 $('.button--modal-open').on('click', function () {
